0fc0ad15954563a525312b2bda2c6cda4c7fbbc2
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-07-08 06:41:54 +0900

    add some images for README

---
+++ docs/precache-manifest.09317bc0ae4f2149a013924de8edcd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce7925559175fe5d6648d3c1e8391dc",
+    "url": "/form-template/index.html"
+  },
+  {
+    "revision": "86c5625c333748797640",
+    "url": "/form-template/static/css/main.b13d2814.chunk.css"
+  },
+  {
+    "revision": "10c3070d1a7361215c60",
+    "url": "/form-template/static/js/2.19f4c5fd.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/form-template/static/js/2.19f4c5fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c5625c333748797640",
+    "url": "/form-template/static/js/main.807497e7.chunk.js"
+  },
+  {
+    "revision": "e4d2e84c4251843048e5",
+    "url": "/form-template/static/js/runtime-main.6032b3d9.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63e26d9683dcf80840b146ed663c911a.js"
+  "/form-template/precache-manifest.09317bc0ae4f2149a013924de8edcd7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/form-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
