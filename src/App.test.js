3e2d0f4599ec961cde9fef67e53092d75af45f30
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-08-18 17:49:44 +0900

    notes on unit testing and integration testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,16 @@
 // Jest is a Test Runner and an Assertion Library
 
-// Assertion: expect things to exist about our code ("sky is blue")
+// Assertion: expect things to be a certain way in our code ("sky is blue")
 // Mocks: fake something (data, function), using fake tests for smaller chunks, substitute for API call, etc
 // Snapshot: take a snapshot of our code result dirctly (e.g. React DOM output) and check if it's equal to what we assert
 
-test("First test", () => {
+// Testing needs to be in place for applications that are growing in scale
+// Easier to write the tests as you are writing the code
+
+// Unit test
+// It only tests one thing (one specific unit)
+
+test("Trial test", () => {
   expect(true).toBeTruthy();
 });
 
@@ -12,3 +18,10 @@ test("First test", () => {
 // test("First test", () => {
 //   expect(false).toBeTruthy();
 // });
+
+// Integratino test
+// More about coding working together
+// e.g. one functino goes off an runs another function runs another..
+
+// In React only renders a single level of component, not its children
+// this is why we should use primarily integration tests
