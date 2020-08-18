9d2687646bb863b8f9b3b5f7d04c54356b34d1c1
Anthony Avila <hellowhirl@users.noreply.github.com>
2020-08-18 16:19:25 +0900

    jest fundamentals

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// Jest is a Test Runner and an Assertion Library
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Assertion: expect things to exist about our code ("sky is blue")
+// Mocks: fake something (data, function), using fake tests for smaller chunks, substitute for API call, etc
+// Snapshot: take a snapshot of our code result dirctly (e.g. React DOM output) and check if it's equal to what we assert
+
+test("First test", () => {
+  expect(true).toBeTruthy();
 });
+
+// below test will fail
+// test("First test", () => {
+//   expect(false).toBeTruthy();
+// });
